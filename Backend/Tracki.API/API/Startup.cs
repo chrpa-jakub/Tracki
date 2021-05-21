f9a9fd7bb97bbc365744b1e490ff15af1b732980
Michal Kopecky <kopeckymichal@sssvt.cz>
2021-05-21 22:35:27 +0200

    Added basic login and register with jwt tokens

--- Backend/Tracki.API/API/Startup.cs
+++ Backend/Tracki.API/API/Startup.cs
@@ -1,3 +1,5 @@
+using API.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -8,12 +10,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using TrackiBackEnd.Model;
 
 namespace API
 {
@@ -39,13 +42,26 @@ namespace API
 			services.AddIdentity<IdentityUser, IdentityRole>(options =>
 					options.Password = new PasswordOptions
 					{
-						RequireDigit = true,
-						RequiredLength = 8,
-						RequireLowercase = true,
-						RequireUppercase = true,
+						RequireDigit = false,
+						RequiredLength = 6,
+						RequireLowercase = false,
+						RequireUppercase = false,
 						RequireNonAlphanumeric = false
 					})
 				.AddEntityFrameworkStores<ApplicationDbContext>();
+
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+			   .AddJwtBearer(options =>
+			   options.TokenValidationParameters = new TokenValidationParameters
+			   {
+				   ValidateIssuer = false,
+				   ValidateAudience = false,
+				   ValidateLifetime = true,
+				   ValidateIssuerSigningKey = true,
+				   IssuerSigningKey = new SymmetricSecurityKey(
+				   Encoding.UTF8.GetBytes(Configuration["jwt:key"])),
+				   ClockSkew = TimeSpan.Zero
+			   });
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,7 +70,7 @@ namespace API
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
-                app.UseSwagger();
+				app.UseSwagger();
 				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
 			}
 
@@ -64,7 +80,6 @@ namespace API
 
 			app.UseAuthorization();
 			app.UseAuthentication();
-			//app.UseIdentityServer();
 
 			app.UseEndpoints(endpoints =>
 			{
