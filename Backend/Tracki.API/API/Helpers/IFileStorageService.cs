166b78a679742f733e57f135b6316ed76cead775
Jakub Chrpa <ChrpaJakub@sssvt.cz>
2021-06-05 17:59:35 +0200

    Azure storage added.

---
+++ Backend/Tracki.API/API/Helpers/IFileStorageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    
+        public interface IFileStorageService
+        {
+            Task<string> EditFile(byte[] content, string extension, string containerName, string fileRoute);
+            Task DeleteFile(string fileRoute, string containerName);
+            Task<string> SaveFile(byte[] content, string extension, string containerName);
+        }
+}
